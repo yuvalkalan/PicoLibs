a81f735f939d0b90ccbaa89e378814d247d508af
yuvalkalan <yuvalkalan@gmail.com>
2026-03-21 16:04:12 +0200

    add cc1101 and serialin

---
+++ SerialIn/src/SerialIn.cpp
@@ -0,0 +1,35 @@
+#include "SerialIn.h"
+
+SerialIn::SerialIn() : m_message("")
+{
+}
+void SerialIn::reset_bootsel()
+{
+    reset_usb_boot(0, 0);
+}
+void SerialIn::reboot()
+{
+    watchdog_enable(0, 0);
+    while (true)
+        tight_loop_contents();
+}
+void SerialIn::update()
+{
+    // Check to see if anything is available in the serial receive buffer
+    tud_task();
+    while (tud_cdc_available())
+    {
+        char chr = (char)tud_cdc_read_char();
+        if (chr == CTRL_C)
+            reset_bootsel();
+        else if (chr == CTRL_D)
+            reboot();
+        // printf("char is %c (value=%d)\n", chr, chr);
+        m_message += chr;
+    }
+    if (m_message.length() != 0)
+    {
+        // process_command(settings);
+        m_message = "";
+    }
+}
