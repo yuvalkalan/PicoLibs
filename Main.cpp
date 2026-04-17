e3fc5a9a4446d3f113d718f013a895cb69f9e4ce
yuvalkalan <yuvalkalan@gmail.com>
2026-04-17 18:57:50 +0300

    change to pico 2

--- Main.cpp
+++ Main.cpp
@@ -2,18 +2,20 @@
 #include "ConnectCC1101.h"
 #include "UniqueID.h"
 #include "Logger.h"
+#include "LedControl.h"
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 
-pico_unique_board_id_t CLIENT_ID = {{0xE6, 0x61, 0x41, 0x04, 0x03, 0x5C, 0xBE, 0x2D}};
-pico_unique_board_id_t SERVER_ID = {{0xE6, 0x62, 0x5C, 0x05, 0xE7, 0x55, 0x24, 0x2A}};
+pico_unique_board_id_t CLIENT_ID = {{0xA2, 0x04, 0xC4, 0x42, 0xD4, 0x33, 0x1A, 0x97}};
+pico_unique_board_id_t SERVER_ID = {{0xC4, 0xF3, 0xB0, 0x07, 0xB8, 0xAB, 0xC3, 0xE8}};
 
 #define SERVER_ADDRESS 0x01
 #define CLIENT_ADDRESS 0x02
 
 ConnectCC1101 *g_radio = nullptr;
+LedControl led;
 
 void core1_main()
 {
@@ -23,6 +25,7 @@ void core1_main()
         serial_in.update();
         if (g_radio && g_radio->is_connected())
             g_radio->update();
+        led.update();
     }
 }
 
@@ -36,38 +39,46 @@ void run_server()
     Logger::print(LogLevel::DEBUG, "Running server...\n");
     ConnectCC1101 radio(CC1101_FREQ_434MHZ, 0x5, 0x00, SERVER_ADDRESS);
     g_radio = &radio;
-    while (!radio.is_connected())
-    {
-        if (radio.accept(5000))
-        {
-            Logger::print(LogLevel::INFO, "Client connected!\n");
-        }
-        else
-        {
-            Logger::print(LogLevel::WEAK_WARNING, "No connection attempts received. Retrying...\n");
-        }
-    }
     while (true)
     {
-        if (radio.have_data())
+        led.set_blink_speed(LED_BLINK_SLOW);
+        while (!radio.is_connected())
         {
-            Msg msg;
-            if (radio.receive(msg, 5000))
+            if (radio.accept(5000))
             {
-
-                // Logger::print(LogLevel::DEBUG, "Received message (%d bytes): %s\n", msg.length, (char *)msg.data);
-                // remove padding from the msg and print it
-                Logger::print(LogLevel::INFO, "Received message: %s\n", (char *)msg.data + msg.length - 5);
+                Logger::print(LogLevel::INFO, "Client connected!\n");
             }
             else
             {
-                Logger::print(LogLevel::ERROR, "Timeout or error while receiving message payload.\n");
-                break;
+                Logger::print(LogLevel::WEAK_WARNING, "No connection attempts received. Retrying...\n");
             }
         }
+        led.set_blink_speed(LED_BLINK_FAST);
+        auto time_since_last_msg_ms = to_ms_since_boot(get_absolute_time());
+        while (radio.is_connected())
+        {
+            if (radio.have_data())
+            {
+                Msg msg;
+                if (radio.receive(msg, 5000))
+                {
+
+                    // Logger::print(LogLevel::DEBUG, "Received message (%d bytes): %s\n", msg.length, (char *)msg.data);
+                    // remove padding from the msg and print it
+                    Logger::print(LogLevel::INFO, "Received message: %s\n", (char *)msg.data + msg.length - 5);
+                    time_since_last_msg_ms = to_ms_since_boot(get_absolute_time());
+                }
+                else
+                {
+                    Logger::print(LogLevel::ERROR, "Timeout or error while receiving message payload.\n");
+                    break;
+                }
+            }
+            else if (to_ms_since_boot(get_absolute_time()) - time_since_last_msg_ms > 10000)
+                radio.disconnect();
+            sleep_ms(1);
+        }
     }
-    while (true)
-        sleep_ms(1);
 }
 
 void run_client()
@@ -75,28 +86,32 @@ void run_client()
     Logger::print(LogLevel::TRACE, "Running client...\n");
     ConnectCC1101 radio(CC1101_FREQ_434MHZ, 0x5, 0x00, CLIENT_ADDRESS);
     g_radio = &radio;
-    int counter = 1;
-    while (!radio.is_connected())
-    {
-        if (radio.connect(SERVER_ADDRESS, 3000))
-        {
-            Logger::print(LogLevel::INFO, "connected!\n");
-        }
-        else
-        {
-            Logger::print(LogLevel::WEAK_WARNING, "Connection attempt failed. Retrying in 1 second...\n");
-            sleep_ms(1000);
-        }
-    }
-    auto start_time = to_ms_since_boot(get_absolute_time());
     while (true)
     {
-        // every x second, send a message to the server with the current counter value, padded to 1000 bytes
-        // if (radio.is_idle())
-        if (to_ms_since_boot(get_absolute_time()) - start_time >= 1000)
+        int counter = 1;
+        led.set_blink_speed(LED_BLINK_SLOW);
+        while (!radio.is_connected())
         {
-            for (size_t i = 0; i < 5; i++)
+            if (radio.connect(SERVER_ADDRESS, 3000))
             {
+                Logger::print(LogLevel::INFO, "connected!\n");
+            }
+            else
+            {
+                Logger::print(LogLevel::WEAK_WARNING, "Connection attempt failed. Retrying in 1 second...\n");
+                sleep_ms(1000);
+            }
+        }
+        auto start_time = to_ms_since_boot(get_absolute_time());
+        led.set_blink_speed(LED_BLINK_FAST);
+        while (radio.is_connected())
+        {
+            // every x second, send a message to the server with the current counter value, padded to 1000 bytes
+            if (radio.is_idle())
+            // if (to_ms_since_boot(get_absolute_time()) - start_time >= 1000)
+            {
+                // for (size_t i = 0; i < 5; i++)
+                // {
                 Msg msg;
                 std::string data = std::to_string(counter++);
                 // uint string_length = get_rand_32() % 1000;
@@ -106,19 +121,22 @@ void run_client()
                 Logger::print(LogLevel::INFO, "Sending message %d (%d)...\n", counter - 1, msg.length);
                 radio.send(msg);
                 start_time = to_ms_since_boot(get_absolute_time());
+                // }
             }
+            sleep_ms(1);
         }
     }
-    while (true)
-        sleep_ms(1);
 }
 
 int main()
 {
+
     stdio_init_all();
     wait_for_serial(10000);
     multicore_launch_core1(core1_main);
     Logger::set_level(LogLevel::TRACE);
+    Logger::print(LogLevel::DEBUG, "Starting!\n");
+    print_id();
     if (is_server())
     {
         run_server();
