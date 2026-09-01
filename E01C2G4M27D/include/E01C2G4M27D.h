e23fb0e787904c1c6a67cdddfc502cae2ddd6663
yuvalkalan <yuvalkalan@gmail.com>
2026-09-01 18:53:03 +0300

    use highspi with E01C2G4M27D

--- E01C2G4M27D/include/E01C2G4M27D.h
+++ E01C2G4M27D/include/E01C2G4M27D.h
@@ -1,57 +1,53 @@
 #pragma once
+#include "HighSPI.h"
+#include "hardware/timer.h"
+#include <string.h>
+#include "E01C2G4M27D_REG.h"
 
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
+#define SPI_SPEED 10'000'000 // 10MHZ
 
-class E01C2G4M27D {
+class E01C2G4M27D : public HighSPI
+{
+private:
+    uint m_ce, m_irq;
+    uint8_t m_payload_size;
+
 public:
     // Constructor maps the hardware SPI instance and GPIO pins
-    E01C2G4M27D(spi_inst_t* spi_port,  uint pin_ce, uint pin_csn, uint pin_sck, uint pin_mosi, uint pin_miso, uint pin_irq );
+    E01C2G4M27D(spi_inst_t *spi, uint ce, uint csn, uint sck, uint mosi, uint miso, uint irq);
 
     void begin();
-    
+
     // Configuration
-    void setChannel(uint8_t channel);       // 0-125
-    void setPALevel(uint8_t level);         // 0: Min, 3: Max
-    void setDataRate(uint8_t rate);         // 0: 1Mbps, 1: 2Mbps, 2: 250kbps
-    
+    void set_channel(uint8_t channel); // 0-125
+    void set_PA_level(uint8_t level);  // 0: Min, 3: Max
+    void set_data_rate(uint8_t rate);  // 0: 1Mbps, 1: 2Mbps, 2: 250kbps
+
     // Pipe addressing
-    void openWritingPipe(const uint8_t* address);
-    void openReadingPipe(uint8_t number, const uint8_t* address);
-    
+    void open_writing_pipe(const uint8_t *address);
+    void open_reading_pipe(uint8_t number, const uint8_t *address);
+
     // Operation
-    void startListening();
-    void stopListening();
-    
+    void start_listening();
+    void stop_listening();
+
     // Data transfer
-    bool write(const void* buf, uint8_t len);
+    bool write(const void *buf, uint8_t len);
     bool available();
-    void read(void* buf, uint8_t len);
+    void read(void *buf, uint8_t len);
 
     // Quality information
-    uint8_t getObserveTx(); // Returns Reg 0x08 (PLOS_CNT and ARC_CNT)
-    bool getRPD();          // Returns Reg 0x09 bit 0 (Received Power Detector)
+    uint8_t get_observe_tx(); // Returns Reg 0x08 (PLOS_CNT and ARC_CNT)
+    bool get_RPD();           // Returns Reg 0x09 bit 0 (Received Power Detector)
 
-private:
-    spi_inst_t* spi;
-    uint pin_miso, pin_csn, pin_sck, pin_mosi, pin_ce, pin_irq;
-    uint8_t payload_size;
-
-    // GPIO helpers
-    void csnLow();
-    void csnHigh();
-    void ceLow();
-    void ceHigh();
-
-    // SPI Operations
-    uint8_t readRegister(uint8_t reg);
-    void writeRegister(uint8_t reg, uint8_t value);
-    void readRegisterMulti(uint8_t reg, uint8_t* buf, uint8_t len);
-    void writeRegisterMulti(uint8_t reg, const uint8_t* buf, uint8_t len);
-    uint8_t spiTransfer(uint8_t data);
-    
-    // Module controls
-    void flushTx();
-    void flushRx();
-    uint8_t getStatus();
+private: // helper functions (use as macros)
+    __always_inline uint8_t readRegister(uint8_t reg) { return read_single_byte(CMD_R_REGISTER | (reg & 0x1F)); }
+    __always_inline void writeRegister(uint8_t reg, uint8_t data) { write_single_byte(CMD_W_REGISTER | (reg & 0x1F), data); }
+    __always_inline void readRegisterMulti(uint8_t reg, uint8_t *buffer, size_t bytes) { read_burst(CMD_R_REGISTER | (reg & 0x1F), buffer, bytes); }
+    __always_inline void writeRegisterMulti(uint8_t reg, const uint8_t *buffer, size_t bytes) { write_burst(CMD_W_REGISTER | (reg & 0x1F), buffer, bytes); }
+    __always_inline uint8_t get_status() { return strobe(0xFF); }
+    __always_inline void ceLow() { gpio_put(m_ce, 0); }
+    __always_inline void ceHigh() { gpio_put(m_ce, 1); }
+    __always_inline void flush_tx() { strobe(CMD_FLUSH_TX); }
+    __always_inline void flush_rx() { strobe(CMD_FLUSH_RX); }
 };
--- E01C2G4M27D/src/E01C2G4M27D.cpp
+++ E01C2G4M27D/src/E01C2G4M27D.cpp
@@ -1,134 +1,117 @@
 #include "E01C2G4M27D.h"
-#include "E01C2G4M27D_REG.h"
-#include "hardware/timer.h"
-#include <string.h>
 
-E01C2G4M27D::E01C2G4M27D(spi_inst_t *spi_port, uint pin_ce, uint pin_csn, uint pin_sck, uint pin_mosi, uint pin_miso, uint pin_irq)
-    : spi(spi_port),
-      pin_ce(pin_ce),
-      pin_csn(pin_csn),
-      pin_sck(pin_sck),
-      pin_mosi(pin_mosi),
-      pin_miso(pin_miso),
-      pin_irq(pin_irq),
-      payload_size(32)
+E01C2G4M27D::E01C2G4M27D(spi_inst_t *spi, uint ce, uint csn, uint sck, uint mosi, uint miso, uint irq)
+    : HighSPI(spi, miso, csn, sck, mosi, SPI_SPEED), m_ce(ce), m_irq(irq), m_payload_size(32)
 {
 }
 
 void E01C2G4M27D::begin()
 {
-    // Initialize hardware SPI at 4MHz
-    spi_init(spi, 4000000);
-    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-
-    gpio_set_function(pin_miso, GPIO_FUNC_SPI);
-    gpio_set_function(pin_sck, GPIO_FUNC_SPI);
-    gpio_set_function(pin_mosi, GPIO_FUNC_SPI);
-
-    // CSN (Chip Select Not) setup
-    gpio_init(pin_csn);
-    gpio_set_dir(pin_csn, GPIO_OUT);
-    csnHigh();
-
-    // CE (Chip Enable) setup
-    gpio_init(pin_ce);
-    gpio_set_dir(pin_ce, GPIO_OUT);
+    // Initialize hardware SPI
+    init_spi();
+    spi_set_format(m_spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    // CE setup
+    gpio_init(m_ce);
+    gpio_set_dir(m_ce, GPIO_OUT);
     ceLow();
+
+    // TODO: add irq here
 
     sleep_ms(5); // Wait for module to settle on boot
 
     // Configuration settings
-    writeRegister(REG_CONFIG, 0x0C);              // 16-bit CRC, PWR_UP = 0
-    writeRegister(REG_EN_AA, 0x3F);               // Auto-Ack on all pipes
-    writeRegister(REG_EN_RXADDR, 0x03);           // Enable Data Pipes 0 & 1
-    writeRegister(REG_SETUP_AW, 0x03);            // 5-byte address width
-    writeRegister(REG_SETUP_RETR, (5 << 4) | 15); // Auto retransmit: 1500us delay, 15 retries
+    writeRegister(E01C2G4M27D_REG_CONFIG, 0x0C);                                             // 16-bit CRC, PWR_UP = 0
+    writeRegister(E01C2G4M27D_REG_EN_AA, 0x3F);                                              // Auto-Ack on all pipes
+    writeRegister(E01C2G4M27D_REG_EN_RXADDR, 0x03);                                          // Enable Data Pipes 0 & 1
+    writeRegister(E01C2G4M27D_REG_SETUP_AW, 0x03);                                           // 5-byte address width
+    writeRegister(E01C2G4M27D_REG_SETUP_RETR, (RETRANSMIT_DELAY << 4) | RETRANSMIT_RETRIES); // configure Auto retransmit
 
     // Power up in RX mode
-    writeRegister(REG_CONFIG, readRegister(REG_CONFIG) | 0x02 | 0x01);
+    writeRegister(E01C2G4M27D_REG_CONFIG, readRegister(E01C2G4M27D_REG_CONFIG) | 0x02 | 0x01);
     sleep_ms(2);
 }
 
-void E01C2G4M27D::setChannel(uint8_t channel)
+void E01C2G4M27D::set_channel(uint8_t channel)
 {
-    writeRegister(REG_RF_CH, channel & 0x7F);
+    writeRegister(E01C2G4M27D_REG_RF_CH, channel & 0x7F);
 }
 
-void E01C2G4M27D::setPALevel(uint8_t level)
+void E01C2G4M27D::set_PA_level(uint8_t level)
 {
-    uint8_t setup = readRegister(REG_RF_SETUP) & ~0x06;
+    uint8_t setup = readRegister(E01C2G4M27D_REG_RF_SETUP) & ~0x06;
     if (level > 3)
         level = 3;
     setup |= (level << 1);
-    writeRegister(REG_RF_SETUP, setup);
+    writeRegister(E01C2G4M27D_REG_RF_SETUP, setup);
 }
 
-void E01C2G4M27D::setDataRate(uint8_t rate)
+void E01C2G4M27D::set_data_rate(uint8_t rate)
 {
-    uint8_t setup = readRegister(REG_RF_SETUP) & ~0x28; // Clear bits 5 (250k) and 3 (2M)
+    uint8_t setup = readRegister(E01C2G4M27D_REG_RF_SETUP) & ~0x28; // Clear bits 5 (250k) and 3 (2M)
     if (rate == 1)
         setup |= (1 << 3); // 2Mbps
     else if (rate == 2)
         setup |= (1 << 5); // 250kbps
-    writeRegister(REG_RF_SETUP, setup);
+    writeRegister(E01C2G4M27D_REG_RF_SETUP, setup);
 }
 
-void E01C2G4M27D::openWritingPipe(const uint8_t *address)
+void E01C2G4M27D::open_writing_pipe(const uint8_t *address)
 {
-    writeRegisterMulti(REG_TX_ADDR, address, 5);
-    writeRegisterMulti(REG_RX_ADDR_P0, address, 5); // Required for auto-ack packets
-    writeRegister(REG_RX_PW_P0, payload_size);
+    writeRegisterMulti(E01C2G4M27D_REG_TX_ADDR, address, 5);
+    writeRegisterMulti(E01C2G4M27D_REG_RX_ADDR_P0, address, 5); // Required for auto-ack packets
+    writeRegister(E01C2G4M27D_REG_RX_PW_P0, m_payload_size);
 }
 
-void E01C2G4M27D::openReadingPipe(uint8_t number, const uint8_t *address)
+void E01C2G4M27D::open_reading_pipe(uint8_t number, const uint8_t *address)
 {
     if (number == 0)
     {
-        writeRegisterMulti(REG_RX_ADDR_P0, address, 5);
-        writeRegister(REG_RX_PW_P0, payload_size);
+        writeRegisterMulti(E01C2G4M27D_REG_RX_ADDR_P0, address, 5);
+        writeRegister(E01C2G4M27D_REG_RX_PW_P0, m_payload_size);
     }
     else if (number == 1)
     {
-        writeRegisterMulti(REG_RX_ADDR_P1, address, 5);
-        writeRegister(REG_RX_PW_P1, payload_size);
+        writeRegisterMulti(E01C2G4M27D_REG_RX_ADDR_P1, address, 5);
+        writeRegister(E01C2G4M27D_REG_RX_PW_P1, m_payload_size);
     }
 
-    uint8_t en_rxaddr = readRegister(REG_EN_RXADDR);
+    uint8_t en_rxaddr = readRegister(E01C2G4M27D_REG_EN_RXADDR);
     en_rxaddr |= (1 << number);
-    writeRegister(REG_EN_RXADDR, en_rxaddr);
+    writeRegister(E01C2G4M27D_REG_EN_RXADDR, en_rxaddr);
 }
 
-void E01C2G4M27D::startListening()
+void E01C2G4M27D::start_listening()
 {
-    writeRegister(REG_CONFIG, readRegister(REG_CONFIG) | 0x01); // SET PRIM_RX
-    writeRegister(REG_STATUS, 0x70);                            // Clear interrupts
+    writeRegister(E01C2G4M27D_REG_CONFIG, readRegister(E01C2G4M27D_REG_CONFIG) | 0x01); // SET PRIM_RX
+    writeRegister(E01C2G4M27D_REG_STATUS, 0x70);                                        // Clear interrupts
     ceHigh();
     sleep_us(130); // Allow PLL to settle
 }
 
-void E01C2G4M27D::stopListening()
+void E01C2G4M27D::stop_listening()
 {
     ceLow();
     sleep_us(130);
-    flushTx();
-    flushRx();
-    writeRegister(REG_CONFIG, readRegister(REG_CONFIG) & ~0x01); // CLEAR PRIM_RX
+    flush_tx();
+    flush_rx();
+    writeRegister(E01C2G4M27D_REG_CONFIG, readRegister(E01C2G4M27D_REG_CONFIG) & ~0x01); // CLEAR PRIM_RX
 }
 
 bool E01C2G4M27D::write(const void *buf, uint8_t len)
 {
-    stopListening();
+    stop_listening();
 
-    csnLow();
-    spiTransfer(CMD_W_TX_PAYLOAD);
-    spi_write_blocking(spi, (const uint8_t *)buf, len);
+    uint8_t cmd = E01C2G4M27D_CMD_W_TX_PAYLOAD;
+    select();
+    spi_write_blocking(m_spi, &cmd, 1);
+    spi_write_blocking(m_spi, (const uint8_t *)buf, len);
     // Pad remaining payload space with zeros
-    if (payload_size > len)
+    if (m_payload_size > len)
     {
         uint8_t padding[32] = {0};
-        spi_write_blocking(spi, padding, payload_size - len);
+        spi_write_blocking(m_spi, padding, m_payload_size - len);
     }
-    csnHigh();
+    deselect();
 
     // Pulse CE to transmit
     ceHigh();
@@ -137,10 +120,10 @@ bool E01C2G4M27D::write(const void *buf, uint8_t len)
 
     // Wait for TX Data Sent (TX_DS) or Max Retries (MAX_RT) flag
     uint8_t status = 0;
-    uint32_t timeout = to_ms_since_boot(get_absolute_time()) + 15;
+    uint32_t timeout = to_ms_since_boot(get_absolute_time()) + (RETRANSMIT_RETRIES * RETRANSMIT_DELAY_US) / 1000;
     while (true)
     {
-        status = getStatus();
+        status = get_status();
         if (status & 0x30)
             break; // 0x20 = TX_DS, 0x10 = MAX_RT
         if (to_ms_since_boot(get_absolute_time()) > timeout)
@@ -148,11 +131,11 @@ bool E01C2G4M27D::write(const void *buf, uint8_t len)
         sleep_us(50);
     }
 
-    writeRegister(REG_STATUS, 0x30); // Clear interrupt flags
+    writeRegister(E01C2G4M27D_REG_STATUS, 0x30); // Clear interrupt flags
 
     if (status & 0x10)
     {
-        flushTx();
+        flush_tx();
         return false; // Retries exhausted
     }
     return (status & 0x20) != 0; // Success if Data Sent is high
@@ -160,105 +143,38 @@ bool E01C2G4M27D::write(const void *buf, uint8_t len)
 
 bool E01C2G4M27D::available()
 {
-    uint8_t fifo_status = readRegister(REG_FIFO_STATUS);
+    uint8_t fifo_status = readRegister(E01C2G4M27D_REG_FIFO_STATUS);
     if (!(fifo_status & 0x01))
-    {                                    // 0x01 is RX_EMPTY
-        writeRegister(REG_STATUS, 0x40); // Clear RX_DR interrupt
+    {                                                // 0x01 is RX_EMPTY
+        writeRegister(E01C2G4M27D_REG_STATUS, 0x40); // Clear RX_DR interrupt
         return true;
     }
     return false;
 }
 
-uint8_t E01C2G4M27D::getObserveTx()
+uint8_t E01C2G4M27D::get_observe_tx()
 {
-    return readRegister(REG_OBSERVE_TX);
+    return readRegister(E01C2G4M27D_REG_OBSERVE_TX);
 }
 
-bool E01C2G4M27D::getRPD()
+bool E01C2G4M27D::get_RPD()
 {
     // Returns true if the received signal was stronger than -64dBm
-    return (readRegister(REG_RPD) & 0x01) != 0;
+    return (readRegister(E01C2G4M27D_REG_RPD) & 0x01) != 0;
 }
 
 void E01C2G4M27D::read(void *buf, uint8_t len)
 {
-    csnLow();
-    spiTransfer(CMD_R_RX_PAYLOAD);
-    spi_read_blocking(spi, 0xFF, (uint8_t *)buf, len);
 
+    uint8_t cmd = E01C2G4M27D_CMD_R_RX_PAYLOAD;
+    select();
+    spi_write_blocking(m_spi, &cmd, 1);
+    spi_read_blocking(m_spi, 0xFF, (uint8_t *)buf, len);
     // Clear out remaining payload bytes from the hardware RX FIFO
-    if (payload_size > len)
+    if (m_payload_size > len)
     {
         uint8_t dummy[32];
-        spi_read_blocking(spi, 0xFF, dummy, payload_size - len);
+        spi_read_blocking(m_spi, 0xFF, dummy, m_payload_size - len);
     }
-    csnHigh();
-}
-
-// Low-Level Helper Implementations
-
-uint8_t E01C2G4M27D::spiTransfer(uint8_t data)
-{
-    uint8_t rx = 0;
-    spi_write_read_blocking(spi, &data, &rx, 1);
-    return rx;
-}
-
-void E01C2G4M27D::csnLow() { gpio_put(pin_csn, 0); }
-void E01C2G4M27D::csnHigh() { gpio_put(pin_csn, 1); }
-void E01C2G4M27D::ceLow() { gpio_put(pin_ce, 0); }
-void E01C2G4M27D::ceHigh() { gpio_put(pin_ce, 1); }
-
-uint8_t E01C2G4M27D::readRegister(uint8_t reg)
-{
-    csnLow();
-    spiTransfer(CMD_R_REGISTER | (reg & 0x1F));
-    uint8_t val = spiTransfer(0xFF);
-    csnHigh();
-    return val;
-}
-
-void E01C2G4M27D::writeRegister(uint8_t reg, uint8_t value)
-{
-    csnLow();
-    spiTransfer(CMD_W_REGISTER | (reg & 0x1F));
-    spiTransfer(value);
-    csnHigh();
-}
-
-void E01C2G4M27D::readRegisterMulti(uint8_t reg, uint8_t *buf, uint8_t len)
-{
-    csnLow();
-    spiTransfer(CMD_R_REGISTER | (reg & 0x1F));
-    spi_read_blocking(spi, 0xFF, buf, len);
-    csnHigh();
-}
-
-void E01C2G4M27D::writeRegisterMulti(uint8_t reg, const uint8_t *buf, uint8_t len)
-{
-    csnLow();
-    spiTransfer(CMD_W_REGISTER | (reg & 0x1F));
-    spi_write_blocking(spi, buf, len);
-    csnHigh();
-}
-
-uint8_t E01C2G4M27D::getStatus()
-{
-    csnLow();
-    uint8_t status = spiTransfer(0xFF); // Send NOP
-    csnHigh();
-    return status;
-}
-
-void E01C2G4M27D::flushTx()
-{
-    csnLow();
-    spiTransfer(CMD_FLUSH_TX);
-    csnHigh();
-}
-void E01C2G4M27D::flushRx()
-{
-    csnLow();
-    spiTransfer(CMD_FLUSH_RX);
-    csnHigh();
+    deselect();
 }
