47a1b06ed29e1a6b0b1b43a89730fcf8dc1184a6
yuvalkalan <yuvalkalan@gmail.com>
2026-09-19 22:27:22 +0300

    add the WS2812 library

---
+++ WS2812/include/WS2812.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+
+/**
+ * A C++ library for controlling WS2812 LED strips using PIO and DMA.
+ *
+ * This class implements a non-blocking WS2812 driver. It utilizes a dynamically
+ * allocated buffer to store the color states of all LEDs. The DMA controller
+ * is used to automatically feed pixel data into the PIO state machine, freeing
+ * the CPU from bit-banging and timing constraints.
+ */
+class WS2812
+{
+public:
+    /**
+     * Constructs a WS2812 instance.
+     *
+     * @param pio PIO instance (pio0 or pio1)
+     * @param sm State Machine index (0-3)
+     * @param pin GPIO pin connected to the WS2812 data line
+     * @param dma_chan DMA channel to use for data transfer
+     * @param num_leds Number of LEDs in the WS2812 strip
+     */
+    WS2812(PIO pio, uint sm, uint pin, uint dma_chan, uint num_leds);
+
+    /**
+     * Destroys the WS2812 instance and frees the internal pixel buffer.
+     */
+    ~WS2812();
+
+    /**
+     * Initializes the PIO, DMA, and GPIO peripherals.
+     * Configures the PIO program offset safely across multiple instances.
+     */
+    void init();
+
+    /**
+     * Triggers the DMA to send the current buffer to the LED strip.
+     * This function is non-blocking and returns immediately.
+     */
+    void show();
+
+    /**
+     * Sets the RGB color of a specific LED in the buffer.
+     *
+     * @param index The 0-based index of the LED
+     * @param r Red intensity (0-255)
+     * @param g Green intensity (0-255)
+     * @param b Blue intensity (0-255)
+     */
+    void set_pixel_color(uint index, uint8_t r, uint8_t g, uint8_t b);
+
+    /**
+     * Clears all pixels in the buffer (sets them to off/black).
+     */
+    void clear();
+
+private:
+    PIO m_pio;
+    uint m_sm;
+    uint m_pin;
+    uint m_dma_chan;
+    uint m_num_leds;
+    uint32_t *m_dma_buffer;
+};
---
+++ WS2812/src/WS2812.cpp
@@ -0,0 +1,116 @@
+#include "WS2812.h"
+#include "hardware/clocks.h"
+#include "WS2812.pio.h"
+#include <string.h>
+
+WS2812::WS2812(PIO pio, uint sm, uint pin, uint dma_chan, uint num_leds)
+    : m_pio(pio), m_sm(sm), m_pin(pin), m_dma_chan(dma_chan), m_num_leds(num_leds)
+{
+    m_dma_buffer = new uint32_t[m_num_leds];
+    clear();
+}
+
+WS2812::~WS2812()
+{
+    delete[] m_dma_buffer;
+}
+
+void WS2812::init()
+{
+    // 1. Initialize the PIO
+    // using static variable to ensure the program is only added once per PIO instance
+    uint8_t pio_number;
+#if PICO_RP2040
+    static uint pio_program_offset[2] = {0, 0};
+
+#elif PICO_RP2350
+    static uint pio_program_offset[3] = {0, 0, 0};
+
+#else
+#error "Target chip is not recognized!"
+#endif
+    if (m_pio == pio0)
+        pio_number = 0;
+    else if (m_pio == pio1)
+        pio_number = 1;
+    else
+    {
+#if PICO_RP2040
+        panic("Error: Invalid PIO instance. Must be pio0 or pio1.");
+
+#elif PICO_RP2350
+        if (m_pio == pio2 && pio_program_offset[2] == 0)
+            pio_number = 2;
+        else
+            panic("Error: Invalid PIO instance. Must be pio0, pio1 or pio2.");
+
+#endif
+    }
+
+    if (pio_program_offset[pio_number] == 0)
+        pio_program_offset[pio_number] = pio_add_program(m_pio, &ws2812_program);
+
+    pio_sm_config c = ws2812_program_get_default_config(pio_program_offset[pio_number]);
+
+    // Set PIO pin directions
+    pio_gpio_init(m_pio, m_pin);
+    pio_sm_set_consecutive_pindirs(m_pio, m_sm, m_pin, 1, true);
+    sm_config_set_sideset_pins(&c, m_pin);
+
+    // Shift out configuration:
+    // shift_right=false (MSB first), autopull=true, 24-bit threshold
+    sm_config_set_out_shift(&c, false, true, 24);
+
+    // 8MHz PIO clock for 800kHz WS2812 (10 cycles per bit)
+    float div = (float)clock_get_hz(clk_sys) / 8000000.0f;
+    sm_config_set_clkdiv(&c, div);
+
+    // Join FIFOs to give the state machine more TX capacity
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+
+    pio_sm_init(m_pio, m_sm, pio_program_offset[pio_number], &c);
+    pio_sm_set_enabled(m_pio, m_sm, true);
+
+    // 2. Initialize the DMA
+    dma_channel_config dma_c = dma_channel_get_default_config(m_dma_chan);
+    channel_config_set_transfer_data_size(&dma_c, DMA_SIZE_32);
+
+    // Increment read address to traverse the buffer, do not increment write address (TX FIFO)
+    channel_config_set_read_increment(&dma_c, true);
+    channel_config_set_write_increment(&dma_c, false);
+
+    // Pace transfers based on PIO TX FIFO
+    channel_config_set_dreq(&dma_c, pio_get_dreq(m_pio, m_sm, true));
+
+    dma_channel_configure(
+        m_dma_chan, &dma_c, &m_pio->txf[m_sm], // Write address (PIO TX FIFO)
+        m_dma_buffer,                          // Read address
+        m_num_leds,                            // Number of transfers
+        false                                  // Start immediately? false
+    );
+}
+
+void WS2812::show()
+{
+    // Wait for the previous DMA transfer to finish before starting a new one
+    dma_channel_wait_for_finish_blocking(m_dma_chan);
+
+    // Set the read address back to the start of the buffer and trigger the transfer
+    dma_channel_set_read_addr(m_dma_chan, m_dma_buffer, true);
+}
+
+void WS2812::set_pixel_color(uint index, uint8_t r, uint8_t g, uint8_t b)
+{
+    if (index >= m_num_leds)
+        return;
+
+    // WS2812 protocol requires GRB order.
+    // Since we configured the PIO for MSB-first out shift with a 24-bit autopull threshold,
+    // we place the 24 bits of color data in the most significant bits of the 32-bit word.
+    m_dma_buffer[index] = ((uint32_t)g << 24) | ((uint32_t)r << 16) | ((uint32_t)b << 8);
+}
+
+void WS2812::clear()
+{
+    memset(m_dma_buffer, 0, m_num_leds * sizeof(uint32_t));
+}
